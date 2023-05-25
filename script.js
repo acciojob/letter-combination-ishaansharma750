ae3f997b26efacd7708c1b941f52eb8adfe0fa60
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-26 01:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+let com =[0,1,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+
+
+	function f(ques,comb,res){
+	
+	    if(ques.length==0){
+	      res.push(comb);
+	      return;
+	    }
+	   
+	    let dig = parseInt(ques[0]);
+	    let curStr = com[dig];
+	
+	    for(let i=0;i<curStr.length;i++){
+	        let curChar=curStr[i];
+	        f(ques.substring(1),comb+curChar,res);
+	    }
+	}
+
+	function letterCombinations(input_digit) {
+	  //Complete the function
+		let res=[];
+	f(input_digit,"",res);
+	return res;
+	}
 
 module.exports = letterCombinations;
